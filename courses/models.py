e0bf9a84d2dca71ccc71b55a150d57909d84d9f4
AideenM12 <a.malone1289@gmail.com>
2021-09-07 16:27:45 +0000

    Update courses/models.py and courses.json

--- courses/models.py
+++ courses/models.py
@@ -7,8 +7,10 @@ class Course(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    start_date = models.CharField(default="01-01-2021", max_length=50)
+    end_date = models.CharField(default="31-12-2021", max_length=50)
+    hours = models.CharField(default="50", max_length=50)
 
     def __str__(self):
         return self.name
