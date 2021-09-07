fe172ea44e9305ca10452f62e7bdeb4ba3334538
AideenM12 <a.malone1289@gmail.com>
2021-09-07 18:18:59 +0000

    Add courses views and template

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Course
 
 # Create your views here.
+
+
+def courses(request):
+    """ A view to return the courses page """
+    courses = Course.objects.all()
+
+    context = {
+        'courses': courses,
+    }
+
+    return render(request, 'courses/courses.html', context)
