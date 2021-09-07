4024ea0d9f0491dc522bdb7417e212a99fe5f75a
AideenM12 <a.malone1289@gmail.com>
2021-09-07 21:25:45 +0000

    Add styles to courses template

--- courses/models.py
+++ courses/models.py
@@ -8,6 +8,7 @@ class Course(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
     start_date = models.CharField(default="01-01-2021", max_length=50)
     end_date = models.CharField(default="31-12-2021", max_length=50)
     hours = models.CharField(default="50", max_length=50)
