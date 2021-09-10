74035025113addf02ac5f792523d459b251da27b
AideenM12 <a.malone1289@gmail.com>
2021-09-10 14:17:40 +0000

    Create exam_courses and exam_course_detail templates

--- exam_courses/urls.py
+++ exam_courses/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.exam_courses, name='exam_courses'),
-    path('<exam_course_id>', views.exam_course_detail, name=(
-        'exam_course_detail')),
+    path('<exam_course_id>', views.exam_course_detail, name='exam_course_detail'),
 ]
--- joyce_school/urls.py
+++ joyce_school/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
     path('courses/', include('courses.urls')),
+    path('exam_courses/', include('exam_courses.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
