fc0676fc3e26acf3052278e8329b8f596b319ac4
AideenM12 <a.malone1289@gmail.com>
2021-09-16 13:34:40 +0000

    Update contexts.py file

--- cart/contexts.py
+++ cart/contexts.py
@@ -12,26 +12,28 @@ def cart_contents(request):
     course_count = 0
     exam_course_count = 0
     cart = request.session.get('cart', {})
+    course = None
+    exam_course = None
 
     for item_id, item_data in cart.items():
         if isinstance(item_data, int):
             course = get_object_or_404(Course, pk=item_id)
             exam_course = get_object_or_404(ExamCourse, pk=item_id)
-            if item_id == course:
-                total += item_data * course.price
-                course_count += item_data
-                cart_items.append({
-                    'item_id': item_id,
-                    'quantity': item_data,
-                    'course': course,
-                })
-            else:
+            if not item_id == course.id:
                 total += item_data * exam_course.price
                 exam_course_count += item_data
                 cart_items.append({
                     'item_id': item_id,
                     'quantity': item_data,
                     'exam_course': exam_course,
+                })
+            else:
+                total += item_data * course.price
+                course_count += item_data
+                cart_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'course': course,
                 })
              
     context = {
