458c02ecf5ef8bb04b719a6a6ae0e6ad6bc62c7f
AideenM12 <a.malone1289@gmail.com>
2021-09-16 11:54:14 +0000

    Create contexts.py file in cart app

---
+++ cart/contexts.py
@@ -0,0 +1,44 @@
+from decimal import Decimal
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+from courses.models import Course
+from exam_courses.models import ExamCourse
+
+
+def cart_contents(request):
+
+    cart_items = []
+    total = 0
+    course_count = 0
+    exam_course_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, item_data in cart.items():
+        if isinstance(item_data, int):
+            course = get_object_or_404(Course, pk=item_id)
+            exam_course = get_object_or_404(ExamCourse, pk=item_id)
+            if item_id == course:
+                total += item_data * course.price
+                course_count += item_data
+                cart_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'course': course,
+                })
+            else:
+                total += item_data * exam_course.price
+                exam_course_count += item_data
+                cart_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'exam_course': exam_course,
+                })
+             
+    context = {
+        'cart_items': cart_items,
+        'total': total,
+        'course_count': course_count,
+        'exam_course_count': exam_course_count
+        }
+
+    return context
