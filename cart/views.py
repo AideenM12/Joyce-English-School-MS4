de238fedfc12113c0b025cbb61cc7837271a7c3b
AideenM12 <a.malone1289@gmail.com>
2021-09-22 19:59:03 +0000

    Add cart checking functionality to add_to_cart view

--- cart/views.py
+++ cart/views.py
@@ -12,35 +12,57 @@ def cart(request):
 
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
-
+    
     quantity = 1
     redirect_url = request.POST.get('redirect_url')
     item_type = request.POST.get('item_type')
     cart = request.session.get('cart', {"courses": {}, "exam_courses": {}})
-
+    
     if request.user.is_authenticated:
-        if cart.items():
-            messages.error(request, 'You already have added a course to your cart.')
-            return redirect('home')
+        if item_type == "courses":
+            if item_id in list(cart['courses'].keys()):
+                cart['courses'][item_id] += quantity
+            else:
+                cart['courses'][item_id] = quantity
+           
+        elif item_type == "exam_courses":
+            if item_id in list(cart['exam_courses'].keys()):
+                cart['exam_courses'][item_id] += quantity
+            else:
+                cart['exam_courses'][item_id] = quantity
 
-    if item_type == "courses":
-        if item_id in list(cart['courses'].keys()):
-            cart['courses'][item_id] += quantity
-        else:
-            cart['courses'][item_id] = quantity
-    elif item_type == "exam_courses":
-        if item_id in list(cart['exam_courses'].keys()):
-            cart['exam_courses'][item_id] += quantity
-        else:
-            cart['exam_courses'][item_id] = quantity
+        if request.user.is_authenticated and cart.items():
+           if item_type == "courses":
+               if item_id in cart == cart['courses'][item_id]:
+                   messages.error(
+                   request, 'You already have added a course to your cart.')
+                   return redirect('home')
 
-    request.session['cart'] = cart
-    print(cart)
-    return redirect(redirect_url)
+        
+        
+    
+       # if item_type == "courses":
+        #    if cart.items():
+         #   if item_id in list(cart['courses'].keys()):
+          #      messages.error(
+           #     request, 'You already have added a course to your cart.')
+            #    return redirect('home')
+        #elif item_type == "exam_courses":
+         #    if item_id in list(cart['exam_courses'].keys()):
+          #      messages.error(
+           #     request, 'You already have added a course to your cart.')
+            #    return redirect('home')
+    #cart[id] = cart.get(id, quantity)
+    
+
+
+        request.session['cart'] = cart
+        print(cart)
+        return redirect(redirect_url)
 
 
 def remove_from_cart(request, item_id):
-    
+
     try:
         course = None
         exam_course = None
@@ -51,20 +73,20 @@ def remove_from_cart(request, item_id):
 
         item_type = request.POST['item_type']
         print(f'item type: {item_type}')
-        
+
         if 'item_type' in request.POST:
             item_type = request.POST['item_type']
             print(item_type)
 
         if item_type == 'course':
-            
+
             if item_id in cart['courses'].keys():
                 del cart['courses'][item_id]
         elif item_type == 'exam_course':
-            
+
             if item_id in cart['exam_courses'].keys():
                 del cart['exam_courses'][item_id]
-               
+
         print(item_type)
         request.session['cart'] = cart
         return HttpResponse(status=200)
@@ -78,5 +100,5 @@ def remove_from_cart(request, item_id):
 def clear_cart(request):
     """Remove courses from the cart"""
     request.session['cart'] = {"courses": {}, "exam_courses": {}}
-  
+
     return redirect('home')
