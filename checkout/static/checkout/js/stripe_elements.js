b67c474d6e6ed607eae833d39e30bbcf67838d01
AideenM12 <a.malone1289@gmail.com>
2021-09-25 13:16:53 +0000

    Create cache_checkout_data view

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -95,9 +95,21 @@ form.addEventListener('submit', function(ev) {
     card.update({ 'disabled': true});
     $('#submit-button').attr('disabled', true);
     stripe.confirmCardPayment(clientSecret, {
-        payment_method: {
-            card: card,
+      payment_method: {
+        card: card,
+        billing_details: {
+            name: $.trim(form.full_name.value),
+            phone: $.trim(form.phone_number.value),
+            email: $.trim(form.email.value),
+            address:{
+                line1: $.trim(form.street_address1.value),
+                line2: $.trim(form.street_address2.value),
+                city: $.trim(form.town_or_city.value),
+                country: $.trim(form.country.value),
+                state: $.trim(form.county.value),
+            }
         }
+        },
     }).then(function(result) {
         if (result.error) {
             var errorDiv = document.getElementById('card-errors');
