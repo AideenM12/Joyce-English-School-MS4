5eea7ec99debf1f29414b7031dbff42454d1e67c
AideenM12 <a.malone1289@gmail.com>
2021-09-29 16:37:45 +0000

    Install and import dj_database_url to settings.py

--- joyce_school/settings.py
+++ joyce_school/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,13 +125,16 @@ WSGI_APPLICATION = 'joyce_school.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#    'default': {
+ #       'ENGINE': 'django.db.backends.sqlite3',
+  #      'NAME': BASE_DIR / 'db.sqlite3',
+   # }
+#}
 
+DATABASES = {
+    'default': dj_database_url.parse('postgres://hutkzwqeagdfml:dd4ac6a541505293b21bd65057c7d60406fbc03699f0055921ac036bbb2713a4@ec2-52-213-119-221.eu-west-1.compute.amazonaws.com:5432/d5ou0643v1tmn0')
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
