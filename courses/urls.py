b8f6f35d9667c4c833c5d120349be1529a956df2
AideenM12 <a.malone1289@gmail.com>
2021-09-29 10:36:51 +0000

    Create edit_course template, view and url

--- courses/urls.py
+++ courses/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.courses, name='courses'),
     path('<int:course_id>/', views.course_detail, name='course_detail'),
     path('add_course/', views.add_course, name='add_course'),
+    path('edit/<int:course_id>/', views.edit_course, name='edit_course'),
 ]
