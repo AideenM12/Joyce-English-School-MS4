eef5bf1a3be23fac4f4c5ff2cfab705c1c10b445
AideenM12 <a.malone1289@gmail.com>
2021-10-02 14:18:31 +0000

    Remove secret key and set debug

--- joyce_school/settings.py
+++ joyce_school/settings.py
@@ -22,12 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    'django-insecure-_mdp1bpgj-sr&0a)#8y$_3qgbw@!bd=8)o$bamiv7j)a$q63u('
-)
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['joyce-english-school.herokuapp.com', 'localhost']
 
