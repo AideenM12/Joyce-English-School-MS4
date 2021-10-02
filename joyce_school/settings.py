62ac2f55223586c74ca581495825fb60b034ac5d
AideenM12 <a.malone1289@gmail.com>
2021-10-02 13:50:36 +0000

    Create postgres database

--- joyce_school/settings.py
+++ joyce_school/settings.py
@@ -125,16 +125,14 @@ WSGI_APPLICATION = 'joyce_school.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
- #       'ENGINE': 'django.db.backends.sqlite3',
-  #      'NAME': BASE_DIR / 'db.sqlite3',
-   # }
-#}
-
 DATABASES = {
-    'default': dj_database_url.parse('postgres://hutkzwqeagdfml:dd4ac6a541505293b21bd65057c7d60406fbc03699f0055921ac036bbb2713a4@ec2-52-213-119-221.eu-west-1.compute.amazonaws.com:5432/d5ou0643v1tmn0')
-}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+ }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
