250083cb4b729cf795f1e548f82ea7b97d535caf
AideenM12 <a.malone1289@gmail.com>
2021-10-08 13:05:29 +0000

    Create reviews template and update reviews view

--- reviews/views.py
+++ reviews/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+
+from .models import Review
 
 # Create your views here.
 
+
 def reviews(request):
     """ A view to return the index page """
+    reviews = Review.objects.order_by('-date_created')
 
-    return render(request, 'reviews/reviews.html')
+    context = {
+        'reviews': reviews
+    }
+
+    return render(request, 'reviews/reviews.html', context)
