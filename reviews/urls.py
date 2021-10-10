886817897cb64253bcb9757c7c7f344899571544
AideenM12 <a.malone1289@gmail.com>
2021-10-10 18:03:01 +0000

    Create delete_review url and template

--- reviews/urls.py
+++ reviews/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.reviews, name='reviews'),
     path('write_review/', views.write_review, name='write_review'),
     path('edit_review/<review_id>', views.edit_review, name='edit_review'),
+    path('delete_review/<review_id>', views.delete_review, name='delete_review'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -11,7 +11,6 @@ from profiles.models import UserProfile
 def reviews(request):
     """ A view to return the index page """
     reviews = Review.objects.order_by('-date_created')
-  
 
     context = {
         'reviews': reviews
@@ -20,12 +19,11 @@ def reviews(request):
     return render(request, 'reviews/reviews.html', context)
 
 
-
 def write_review(request):
     """."""
     if request.method == 'POST':
         form = WriteReview(request.POST)
-        
+
         if form.is_valid():
             review = form.save(commit=False)
             review.creator = UserProfile.objects.get(user=request.user)
@@ -33,7 +31,8 @@ def write_review(request):
             messages.success(request, 'Review added!')
             return redirect('write_review')
         else:
-            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+            messages.error(
+                request, 'Failed to add product. Please ensure the form is valid.')
     else:
         form = WriteReview()
 
@@ -47,16 +46,12 @@ def write_review(request):
 def edit_review(request, review_id):
     """"""
     review = get_object_or_404(Review, pk=review_id)
-   
-   # review.creator = UserProfile.objects.get(user=request.user)
-    if UserProfile.objects.get(user=request.user) != review.creator:
-    #if not request.user.is_superuser or if request.user :
-    #    print(request.user)
+
+    if request.user.userprofile != review.creator:
         messages.error(request, 'You do not have access to that Review!')
         return redirect('reviews')
-   # if request.user == review.creator:
+
     if request.method == 'POST':
-                 
         edit_form = WriteReview(request.POST, instance=review)
         if edit_form.is_valid():
             review = edit_form.save(commit=False)
@@ -70,8 +65,17 @@ def edit_review(request, review_id):
     context = {
         'form': edit_form,
         'review': review
-        }
+    }
     return render(request, 'reviews/edit_review.html', context)
-    #else:
-     #   messages.error(request, 'You do not have access to that Review!')
-      #  return redirect('reviews')
+
+
+def delete_review(request, review_id):
+    """ """
+    review = get_object_or_404(Review, pk=review_id)
+
+    if request.user.userprofile == review.creator:
+        review.delete()
+        messages.success(request, 'Review Deleted!')
+        return redirect('reviews')
+    messages.error(request, 'You are not the owner of this review.')
+    return redirect('reviews')
