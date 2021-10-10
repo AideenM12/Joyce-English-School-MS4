6f559c2a421bebc61f4cedb4b83bc9f57a252597
AideenM12 <a.malone1289@gmail.com>
2021-10-10 17:41:50 +0000

    Create edit_review view, url and template

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.reviews, name='reviews'),
     path('write_review/', views.write_review, name='write_review'),
+    path('edit_review/<review_id>', views.edit_review, name='edit_review'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -3,6 +3,8 @@ from django.contrib import messages
 
 from .models import Review
 from .forms import WriteReview
+
+from profiles.models import UserProfile
 # Create your views here.
 
 
@@ -17,8 +19,7 @@ def reviews(request):
 
     return render(request, 'reviews/reviews.html', context)
 
-from .models import Review
-from profiles.models import UserProfile
+
 
 def write_review(request):
     """."""
@@ -41,3 +42,36 @@ def write_review(request):
         'form': form
     }
     return render(request, template, context)
+
+
+def edit_review(request, review_id):
+    """"""
+    review = get_object_or_404(Review, pk=review_id)
+   
+   # review.creator = UserProfile.objects.get(user=request.user)
+    if UserProfile.objects.get(user=request.user) != review.creator:
+    #if not request.user.is_superuser or if request.user :
+    #    print(request.user)
+        messages.error(request, 'You do not have access to that Review!')
+        return redirect('reviews')
+   # if request.user == review.creator:
+    if request.method == 'POST':
+                 
+        edit_form = WriteReview(request.POST, instance=review)
+        if edit_form.is_valid():
+            review = edit_form.save(commit=False)
+            review.creator = UserProfile.objects.get(user=request.user)
+            edit_form.save()
+            messages.success(request, 'Review updated!')
+            return redirect('reviews')
+
+    edit_form = WriteReview(instance=review)
+
+    context = {
+        'form': edit_form,
+        'review': review
+        }
+    return render(request, 'reviews/edit_review.html', context)
+    #else:
+     #   messages.error(request, 'You do not have access to that Review!')
+      #  return redirect('reviews')
