b3df122d557e20bf5df788baf816ac35de9d1c3c
AideenM12 <a.malone1289@gmail.com>
2021-10-11 19:53:33 +0000

    Create back-to-top button on reviews template

--- static/js/script.js
+++ static/js/script.js
@@ -4,6 +4,22 @@ $(document).ready(function(){
     $('.collapsible').collapsible();
     $('.dropdown-trigger').dropdown();
   });
+// Floating Back to Top Button functionality
+// This code was found on https://codepen.io/desirecode/pen/MJPJqV
+
+$(document).ready(function(){ 
+  $(window).scroll(function(){ 
+      if ($(this).scrollTop() > 100) { 
+          $('#scroll').fadeIn(); 
+      } else { 
+          $('#scroll').fadeOut(); 
+      } 
+  }); 
+  $('#scroll').click(function(){ 
+      $("html, body").animate({ scrollTop: 0 }, 600); 
+      return false; 
+  }); 
+});
 
 //This code was taken from Code Institutes Boutique Ado Tutorials
   function handleEnableDisable(itemId) {
