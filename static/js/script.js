1a4f6608f3fb0ea0da39944f16be17e9e45c84d1
AideenM12 <a.malone1289@gmail.com>
2021-10-12 23:17:53 +0000

    Remove commented out JS code

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
+    //Initializes parallax container
     $('.parallax').parallax();
+     //Initializes mobile sidenav
     $('.sidenav').sidenav({edge: "right"});
+    //Initializes collapsible container
     $('.collapsible').collapsible();
+    //Initializes parallax container
     $('.dropdown-trigger').dropdown();
     //Initializes delete modal alerts
     $('.modal').modal();
@@ -22,49 +26,6 @@ $(document).ready(function(){
       return false; 
   }); 
 });
-
-//This code was taken from Code Institutes Boutique Ado Tutorials
-  function handleEnableDisable(itemId) {
-    var currentValue = parseInt($(`#id_qty_${itemId}`).val());
-    var minusDisabled = currentValue < 2;
-    var plusDisabled = currentValue > 10;
-    $(`#decrement-qty_${itemId}`).prop('disabled', minusDisabled);
-    $(`#increment-qty_${itemId}`).prop('disabled', plusDisabled);
-}
-
-// Ensure proper enabling/disabling of all inputs on page load
-var allQtyInputs = $('.qty_input');
-for(var i = 0; i < allQtyInputs.length; i++){
-    var itemId = $(allQtyInputs[i]).data('item_id');
-    handleEnableDisable(itemId);
-}
-
-// Check enable/disable every time the input is changed
-$('.qty_input').change(function() {
-    var itemId = $(this).data('item_id');
-    handleEnableDisable(itemId);
-});
-
-// Increment quantity
-$('.increment-qty').click(function(e) {
-   e.preventDefault();
-   var closestInput = $(this).closest('.input-group').find('.qty_input')[0];
-   var currentValue = parseInt($(closestInput).val());
-   $(closestInput).val(currentValue + 1);
-   var itemId = $(this).data('item_id');
-   handleEnableDisable(itemId);
-});
-
-// Decrement quantity
-$('.decrement-qty').click(function(e) {
-   e.preventDefault();
-   var closestInput = $(this).closest('.input-group').find('.qty_input')[0];
-   var currentValue = parseInt($(closestInput).val());
-   $(closestInput).val(currentValue - 1);
-   var itemId = $(this).data('item_id');
-   handleEnableDisable(itemId);
-});
-
 
 $('.update-link').click(function(e) {
   var form = $(this).prev('.update-form');
